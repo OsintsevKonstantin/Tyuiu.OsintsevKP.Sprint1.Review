41e9c6a0a9d9bf5795484a878ad23b3603b5632a
Tyuiu.OsintsevKP@outlook.com <osintcevkp@mail.ru>
2023-10-20 22:20:35 +0500

    review

--- Tyuiu.OsintsevKP.Sprint1.TaskReview.V12.Lib/DataService.cs
+++ Tyuiu.OsintsevKP.Sprint1.TaskReview.V12.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.OsintsevKP.Sprint1.TaskReview.V12.Lib
         public double Calculate(double x, double y)
         {
             if (x == 1) return -1;
-            else return Math.Pow((x + 1) / (x - 1),x) + 18 * x * Math.Pow(y, 2);
+            else return Math.Round(Math.Pow((x + 1) / (x - 1),x) + 18 * x * Math.Pow(y, 2), 3);
         }
     }
 }
